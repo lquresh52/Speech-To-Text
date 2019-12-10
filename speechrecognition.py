7ba51c865a581827c4bcdd722431ecdb878f7515
Quresh Lakdawala <lquresh52@gmail.com>
2019-12-10 23:23:57 +0530

    First commit

--- speechrecognition.py
+++ speechrecognition.py
@@ -2,7 +2,7 @@ import speech_recognition as sr
 import pyaudio
 r= sr.Recognizer()
 with sr.Microphone() as source:
-    print ("Speek anything")
+    print ("Speek anything : ")
     audio=r.listen(source)
     try:
         text= r.recognize_google(audio)
